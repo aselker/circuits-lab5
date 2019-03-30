ad979e6ca670c28141b4cc4ed16d28e5d5d2237d
Adam Selker <adamselker@gmail.com>
2019-03-30 15:41:21 -0400

    refactor as prep for p-type

--- exp1.py
+++ exp1.py
@@ -23,25 +23,24 @@ def clip_range(xs, ys, bounds):
 
 vg, isat = clip_range(vg, isat, (np.finfo(float).eps, np.inf)) # Clip to positive
 
+def fit(xs, ys, model, initial_params):
+  def err_f(params): return np.mean(np.power(np.log(ys) - np.log(model(xs, params)), 2))
+  res = minimize(err_f, x0 = initial_params, method='Nelder-Mead')
+  print(res)
+  return res.x
+
 # From ekvfit
-def ekv(vg, Is, Vt, kappa):
+def ekv_n(vg, params):
+  Is, Vt, kappa = params
   return Is * np.power(np.log(1 + np.exp(kappa*(vg - Vt)/(2*0.0258))), 2)
 
-# Error is mean-squared diff between theoretical and practice
-def err_f(Is, Vt, kappa): return np.mean(np.power(np.log(isat) - np.log(ekv(np.array(vg), Is, Vt, kappa)), 2))
-
-# Found with ekvfit, but not good enough
-initial_params = [7.2197482429849523e-08, 0.50917435504354447, 3.3782458038598859]
-
-res = minimize(lambda args: err_f(args[0], args[1], args[2]), x0 = initial_params, method='Nelder-Mead')
-print(res)
-params = res.x
+params = fit(vg, isat, ekv_n, [7.2197482429849523e-08, 0.50917435504354447, 3.3782458038598859])
 
 fig = plt.figure(figsize=(8,6))
 ax = plt.subplot(111)
 
 ax.semilogy(vg, isat, 'b.', label="Saturation current (experimental)")
-ax.semilogy(vg, ekv(vg, params[0], params[1], params[2]), 'g-', label="Saturation current (theoretical, Is = %g, Vt = %g, κ = %g)" %  (params[0], params[1], params[2]))
+ax.semilogy(vg, ekv_n(vg, params), 'g-', label="Saturation current (theoretical, Is = %g, Vt0 = %g, κ = %g)" %  (params[0], params[1], params[2]))
 
 plt.title("")
 plt.xlabel("")
